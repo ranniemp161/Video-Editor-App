8a0dd3993e96768e4d32249003615c139b68e07f
rannieperalta <rannieperalta06@gmail.com>
2026-02-07 21:26:04 +0800

    Fix allowedHosts type error

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      allowedHosts: 'all',
+      allowedHosts: true,
       proxy: {
         '/api': {
           target: 'http://localhost:8000',
